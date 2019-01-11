2b011ec283b5b844588daacb97e064956f4a9054
mufeez-amjad <zeefumdajma@outlook.com>
2019-01-10 21:47:38 -0500

    connect to mongodb

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import schema from './schema';
 const app = express();
 const PORT = 3000;
 
+// configure mongodb connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/shopify-backend', { useNewUrlParser: true });
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(err, resp){
+    console.log(resp);
+});
+
 //set default route
 app.get('/', (req, res) => {
     return res.json({
